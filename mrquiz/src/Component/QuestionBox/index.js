c2d968f04c65dc291c13f7e781727b0e62a79137
Marco De Felice <marco.defelice@icloud.com>
2019-02-16 20:11:53 +0100

    init repo

---
+++ mrquiz/src/Component/QuestionBox/index.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './style.scss';
+
+
+class QuestionBox extends Component {
+  render() {
+    return (
+      <div className="QuestionBox">
+        Question Box 
+      </div>
+    );
+  }
+}
+
+export default QuestionBox;
---
+++ mrquiz/src/Component/component.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import QuestionBox from './QuestionBox';
+
+class Component extends Component {
+  render() {
+    return (
+      <QuestionBox />
+    );
+  }
+}
+
+export default Component;
